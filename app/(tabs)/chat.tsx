5f140527212820ada8d025ed2e59fb68c465d8c4
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-01-29 02:25:59 +0000

    finalising styling. added light mode styling with dark as alternative

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -4,14 +4,14 @@ import {
 	ScrollView,
 	KeyboardAvoidingView,
 	Platform,
+	Keyboard,
+	TouchableWithoutFeedback,
 } from "react-native";
 import { Text, View, SafeAreaView } from "@/components/Themed";
-import { useColorScheme } from "@/hooks/useColorScheme";
 import { useState } from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome6";
 
 function ChatBubble({ text, isAi }: { text: string; isAi: boolean }) {
-	const colorScheme = useColorScheme();
-
 	return (
 		<View style={isAi ? styles.aiBubble : styles.userBubble}>
 			<Text>{text}</Text>
@@ -36,43 +36,54 @@ export default function ChatScreen() {
 	};
 
 	return (
-		<SafeAreaView className="flex-1">
-			<KeyboardAvoidingView
-				className="flex-1"
-				behavior={Platform.OS === "ios" ? "padding" : "height"}
-				keyboardVerticalOffset={Platform.OS === "ios" ? 90 : 0}
-			>
-				{messages.length === 0 ? (
-					<View className="flex-1 items-center justify-center px-5">
-						<Text className="text-xl text-center w-2/3 text-red-500">
-							Send a message to start chatting with Wickbot!
-						</Text>
-					</View>
-				) : (
-					<ScrollView className="flex-1 w-full py-1">
-						{messages.map((msg, index) => (
-							<ChatBubble
-								key={index}
-								text={msg.text}
-								isAi={msg.isAi}
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+			<SafeAreaView className="flex-1">
+				<KeyboardAvoidingView
+					className="flex-1"
+					behavior={Platform.OS === "ios" ? "padding" : "height"}
+					keyboardVerticalOffset={Platform.OS === "ios" ? 98 : 0}
+				>
+					{messages.length === 0 ? (
+						<View className="flex-1 items-center justify-center px-5 flex">
+							<FontAwesome
+								name="robot"
+								size="80"
+								className="dark:color-white color-black"
 							/>
-						))}
-					</ScrollView>
-				)}
+							<View
+								darkColor="#a1a1a1"
+								className="h-0.5 w-2/3 bg-gray-400 rounded-lg mb-5 mt-7"
+							></View>
+							<Text className="text-xl text-center w-2/3">
+								Send a message to start chatting with Wickbot!
+							</Text>
+						</View>
+					) : (
+						<ScrollView className="flex-1 w-full py-1">
+							{messages.map((msg, index) => (
+								<ChatBubble
+									key={index}
+									text={msg.text}
+									isAi={msg.isAi}
+								/>
+							))}
+						</ScrollView>
+					)}
 
-				<View className="items-center justify-center border-2 border-[#212631] py-3 px-2">
-					<TextInput
-						className="rounded-xl px-3.5 py-5 w-full bg-gray-700 border border-gray-600 text-white"
-						value={message}
-						onChangeText={setMessage}
-						placeholder="Type a message..."
-						onSubmitEditing={handleSend}
-						returnKeyType="send"
-						inputMode="text"
-					/>
-				</View>
-			</KeyboardAvoidingView>
-		</SafeAreaView>
+					<View className="items-center justify-center bg-gray-500 py-3 px-2">
+						<TextInput
+							className="rounded-2xl px-3.5 py-5 w-full bg-gray-200 dark:bg-gray-700 border border-gray-600 text-black dark:text-white"
+							value={message}
+							onChangeText={setMessage}
+							placeholder="Type a message..."
+							onSubmitEditing={handleSend}
+							returnKeyType="send"
+							inputMode="text"
+						/>
+					</View>
+				</KeyboardAvoidingView>
+			</SafeAreaView>
+		</TouchableWithoutFeedback>
 	);
 }
 
