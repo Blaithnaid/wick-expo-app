0fe08cbbf0eb6e7f66bf272a251b6d6dfa2a175b
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-01-30 12:40:06 +0000

    stripped out placeholders, beginning to fill in Settings

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,8 @@
-import { StyleSheet } from "react-native";
+import { View } from "react-native";
 import { Text, ThemedView } from "@/components/Themed";
 
-export default function TabOneScreen() {
+export default function HomeScreen() {
 	return (
-		<ThemedView className="flex-1 items-center justify-center"></ThemedView>
+		<View className="flex-1 items-center justify-center h-full w-full dark:bg-oxford-500 bg-white"></View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-});
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,35 +1,35 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import { Text } from "@/components/Themed";
+import { View, Pressable } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function SettingsScreen() {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Settings</Text>
-			<View
-				style={styles.separator}
-				lightColor="#eee"
-				darkColor="rgba(255,255,255,0.1)"
-			/>
-			<EditScreenInfo path="app/(tabs)/settings.tsx" />
+		<View className="flex-1 items-center justify-center h-full w-full dark:bg-oxford-500 bg-white">
+			<View className="bg-oxford-300 h-32 w-[95%] rounded-2xl">
+				<Pressable
+					className=" rounded-2xl flex flex-row items-center"
+					android_ripple={{ color: "gray" }}
+					onPressOut={() => {
+						console.log("Pressable pressed");
+					}}
+				>
+					<View className="ml-3 h-24 w-24 bg-white rounded-full overflow-hidden flex justify-center items-center">
+						<FontAwesome
+							name="question-circle"
+							size={90}
+							color="gray"
+						></FontAwesome>
+					</View>
+					<View className="h-8 w-8 bg-pink-600"></View>
+					<View className="h-8 w-8 bg-pink-600"></View>
+				</Pressable>
+			</View>
+			<View>
+				<View className="bg-oxford-500 border-y-2 dark:border-oxford-300 h-16 w-full"></View>
+				<View className="bg-oxford-500 border-y-2 dark:border-oxford-300 h-16 w-full"></View>
+				<View className="bg-oxford-500 border-y-2 dark:border-oxford-300 h-16 w-full"></View>
+				<View className="bg-oxford-500 border-y-2 dark:border-oxford-300 h-16 w-full"></View>
+			</View>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: "80%",
-	},
-});
