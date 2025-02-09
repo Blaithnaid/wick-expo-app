37d6e9edd10c88f3c414ffe019c7b008d0a2e742
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-02-09 15:28:11 +0000

    top card for user

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,35 +1,51 @@
-import { Text } from "@/components/Themed";
-import { View, Pressable } from "react-native";
+import { View, Text } from "@/components/Themed";
+import { router } from "expo-router";
+import { Pressable } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useAuthContext } from "@/services/AuthProvider";
+import { Image } from "expo-image";
 
 export default function SettingsScreen() {
+	// we'll use this to check if the user is logged in
+	// will control the state of the top view
+	const auth = useAuthContext();
+
 	return (
-		<View className="flex-1 items-center justify-center h-full w-full dark:bg-oxford-500 bg-white">
-			<View className="bg-oxford-300 h-32 w-[95%] rounded-2xl">
-				<Pressable
-					className=" rounded-2xl flex flex-row items-center"
-					android_ripple={{ color: "gray" }}
-					onPressOut={() => {
-						console.log("Pressable pressed");
-					}}
-				>
-					<View className="ml-3 h-24 w-24 bg-white rounded-full overflow-hidden flex justify-center items-center">
+		<View className="flex-1 items-center pt-3 h-full w-full">
+			<Pressable
+				className="bg-oxford-300 p-4 w-[95%] rounded-2xl flex flex-row items-center"
+				android_ripple={{ color: "gray" }}
+				// ios equivalent of android_ripple
+				onPress={() => router.push("/(auth)/register")}
+			>
+				<View className="bg-transparent overflow-hidden rounded-full size-16 flex items-center justify-center">
+					{auth.user ? (
+						<Image
+							source={{ uri: auth.user.photoURL }}
+							style={{
+								width: 64,
+								height: 64,
+								borderRadius: 64,
+							}}
+						/>
+					) : (
 						<FontAwesome
-							name="question-circle"
-							size={90}
-							color="gray"
-						></FontAwesome>
-					</View>
-					<View className="h-8 w-8 bg-pink-600"></View>
-					<View className="h-8 w-8 bg-pink-600"></View>
-				</Pressable>
-			</View>
-			<View>
-				<View className="bg-oxford-500 border-y-2 dark:border-oxford-300 h-16 w-full"></View>
-				<View className="bg-oxford-500 border-y-2 dark:border-oxford-300 h-16 w-full"></View>
-				<View className="bg-oxford-500 border-y-2 dark:border-oxford-300 h-16 w-full"></View>
-				<View className="bg-oxford-500 border-y-2 dark:border-oxford-300 h-16 w-full"></View>
-			</View>
+							name="user"
+							size={64}
+							color={"#ffffff"}
+							className="rounded-full"
+						/>
+					)}
+				</View>
+				<View className="pl-2 bg-transparent dark:bg-transparent">
+					<Text className="text-xl text-black dark:text-white">
+						{auth.user ? auth.user.displayName : "Create account"}
+					</Text>
+					<Text className="text-md text-black dark:text-slate-300">
+						{auth.user ? null : "Tap here to sign up or log in!"}
+					</Text>
+				</View>
+			</Pressable>
 		</View>
 	);
 }
