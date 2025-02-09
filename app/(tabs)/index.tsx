3dfa64e21be801775c6359f8bfd56a5468dbb88f
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-02-09 15:28:30 +0000

    remove redundant class

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,6 @@ import { Text, View } from "@/components/Themed";
 
 export default function HomeScreen() {
 	return (
-		<View className="flex-1 items-center justify-center h-full w-full dark:bg-oxford-500 bg-white"></View>
+		<View className="flex-1 items-center justify-center h-full w-full"></View>
 	);
 }
