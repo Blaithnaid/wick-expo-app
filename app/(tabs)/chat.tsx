094aabcdfdaad257e01e4a825b2507811aaa35eb
Blaithnaid <iarlaken@gmail.com>
2025-02-24 11:34:41 +0000

    adding warning msg if not signed in. prevents importing if no active user

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,10 +1,10 @@
 import {
-	TextInput,
-	ScrollView,
-	KeyboardAvoidingView,
-	Platform,
-	Keyboard,
-	TouchableWithoutFeedback,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
 import { httpsCallable } from "firebase/functions";
 import { useFirebaseContext } from "@/services/FirebaseProvider";
@@ -15,121 +15,121 @@ import FontAwesome from "@expo/vector-icons/FontAwesome6";
 import ChatBubble from "@/components/ChatBubble";
 
 export default function ChatScreen() {
-	const [message, setMessage] = useState("");
-	const [messages, setMessages] = useState<{ text: string; role: string }[]>(
-		[]
-	);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<{ text: string; role: string }[]>(
+    []
+  );
 
-	const functions = useFirebaseContext().myFunctions;
-	const chatWithGemini = httpsCallable(functions, "chatWithGemini");
+  const functions = useFirebaseContext().myFunctions;
+  const chatWithGemini = httpsCallable(functions, "chatWithGemini");
 
-	const sendMessage = async (
-		message: string,
-		prevMessages: { text: string; role: string }[]
-	) => {
-		try {
-			const result = await chatWithGemini({
-				prevMessages: prevMessages,
-				userMessage: message,
-			});
+  const sendMessage = async (
+    message: string,
+    prevMessages: { text: string; role: string }[]
+  ) => {
+    try {
+      const result = await chatWithGemini({
+        prevMessages: prevMessages,
+        userMessage: message,
+      });
 
-			return result.data;
-		} catch (error) {
-			console.error("Error calling function:", error);
-		}
-	};
+      return result.data;
+    } catch (error) {
+      console.error("Error calling function:", error);
+    }
+  };
 
-	const scrollViewRef = useRef<ScrollView>(null);
-	const colorScheme = useColorScheme().colorScheme;
+  const scrollViewRef = useRef<ScrollView>(null);
+  const colorScheme = useColorScheme().colorScheme;
 
-	useEffect(() => {
-		const showListener = Keyboard.addListener("keyboardDidShow", () => {
-			scrollViewRef.current?.scrollToEnd({ animated: true });
-		});
-		return () => showListener.remove();
-	}, []);
+  useEffect(() => {
+    const showListener = Keyboard.addListener("keyboardDidShow", () => {
+      scrollViewRef.current?.scrollToEnd({ animated: true });
+    });
+    return () => showListener.remove();
+  }, []);
 
-	const handleSend = async () => {
-		if (message.trim() === "") return;
+  const handleSend = async () => {
+    if (message.trim() === "") return;
 
-		const userMessage = { text: message, role: "user" };
-		setMessages((prev) => [...prev, userMessage]);
-		setMessages((prev) => [...prev, { text: "", role: "model" }]);
+    const userMessage = { text: message, role: "user" };
+    setMessages((prev) => [...prev, userMessage]);
+    setMessages((prev) => [...prev, { text: "", role: "model" }]);
 
-		const response = await sendMessage(message, messages);
-		let aiMessage = { text: (response as string).trim(), role: "model" };
-		setMessages((prev) => [...prev.slice(0, -1), aiMessage]);
+    const response = await sendMessage(message, messages);
+    let aiMessage = { text: (response as string).trim(), role: "model" };
+    setMessages((prev) => [...prev.slice(0, -1), aiMessage]);
 
-		setMessage("");
-	};
+    setMessage("");
+  };
 
-	return (
-		<SafeAreaView className="flex-1">
-			<KeyboardAvoidingView
-				className="flex-1"
-				behavior={Platform.OS === "ios" ? "padding" : "height"}
-				keyboardVerticalOffset={Platform.OS === "ios" ? 98 : 0}
-			>
-				{messages.length === 0 ? (
-					<TouchableWithoutFeedback
-						onPress={() => Keyboard.dismiss()}
-					>
-						<View className="flex-1 items-center justify-center px-5 flex">
-							<FontAwesome
-								name="robot"
-								size={80}
-								color={
-									colorScheme === "dark" ? "white" : "black"
-								}
-							/>
-							<View className="mt-4 mb-3 h-[2px] rounded-full w-[55%] bg-slate-400" />
-							<Text className="text-xl text-center w-2/3">
-								Send a message to start chatting with Wickbot!
-							</Text>
-							<Text className="text-lg text-center w-3/4 mt-4">
-								Click the icon in the top right to get some
-								tips!
-							</Text>
-						</View>
-					</TouchableWithoutFeedback>
-				) : (
-					<ScrollView
-						ref={scrollViewRef}
-						onContentSizeChange={() =>
-							scrollViewRef.current?.scrollToEnd({
-								animated: true,
-							})
-						}
-						keyboardDismissMode="on-drag"
-					>
-						<View
-							className="mt-8 mb-6 h-px w-[90%] self-center dark:bg-oxford-300 bg-oxford-200"
-							lightColor="#eee"
-							darkColor="rgba(255,255,255,0.1)"
-						/>
-						{[...messages].map((msg, index) => (
-							<ChatBubble
-								key={index}
-								text={msg.text}
-								role={msg.role}
-							/>
-						))}
-					</ScrollView>
-				)}
+  return (
+    <SafeAreaView className="flex-1">
+      <KeyboardAvoidingView
+        className="flex-1"
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 98 : 0}
+      >
+        {messages.length === 0 ? (
+          <TouchableWithoutFeedback
+            onPress={() => Keyboard.dismiss()}
+          >
+            <View className="flex-1 items-center justify-center px-5 flex">
+              <FontAwesome
+                name="robot"
+                size={80}
+                color={
+                  colorScheme === "dark" ? "white" : "black"
+                }
+              />
+              <View className="mt-4 mb-3 h-[2px] rounded-full w-[55%] bg-slate-400" />
+              <Text className="text-xl text-center w-2/3">
+                Send a message to start chatting with Wickbot!
+              </Text>
+              <Text className="text-lg text-center w-3/4 mt-4">
+                Click the icon in the top right to get some
+                tips!
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
+        ) : (
+          <ScrollView
+            ref={scrollViewRef}
+            onContentSizeChange={() =>
+              scrollViewRef.current?.scrollToEnd({
+                animated: true,
+              })
+            }
+            keyboardDismissMode="on-drag"
+          >
+            <View
+              className="mt-8 mb-6 h-px w-[90%] self-center dark:bg-oxford-300 bg-oxford-200"
+              lightColor="#eee"
+              darkColor="rgba(255,255,255,0.1)"
+            />
+            {[...messages].map((msg, index) => (
+              <ChatBubble
+                key={index}
+                text={msg.text}
+                role={msg.role}
+              />
+            ))}
+          </ScrollView>
+        )}
 
-				<View className="items-center justify-center py-3 px-2">
-					<TextInput
-						className="rounded-2xl px-3.5 py-5 w-full bg-gray-400 dark:bg-gray-700 border border-gray-600 text-black dark:text-white"
-						value={message}
-						onChangeText={setMessage}
-						onSubmitEditing={handleSend}
-						placeholder="Type a message..."
-						// placeholderTextColor={}
-						returnKeyType="send"
-						inputMode="text"
-					/>
-				</View>
-			</KeyboardAvoidingView>
-		</SafeAreaView>
-	);
+        <View className="items-center justify-center py-3 px-2">
+          <TextInput
+            className="rounded-2xl px-3.5 py-5 w-full bg-gray-400 dark:bg-gray-700 border border-gray-600 text-black dark:text-white"
+            value={message}
+            onChangeText={setMessage}
+            onSubmitEditing={handleSend}
+            placeholder="Type a message..."
+            // placeholderTextColor={}
+            returnKeyType="send"
+            inputMode="text"
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
--- app/(tabs)/igprofile.tsx
+++ app/(tabs)/igprofile.tsx
@@ -3,26 +3,54 @@ import { router } from "expo-router";
 import { Pressable } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useAuthContext } from "@/services/AuthProvider";
+import { useColorScheme } from "@/hooks/useColorScheme";
 import { Image } from "expo-image";
 import { Divider } from "@/components/ui/divider";
 import Importer from "@/components/Importer"
 
 export default function IgProfileScreen() {
-	const auth = useAuthContext();
+  const auth = useAuthContext();
+  const colorScheme = useColorScheme().colorScheme;
 
-	if (!auth.user) {
-		return (
-			<View className="h-full w-full">
+  if (!auth || !auth.profile) {
+    return (
+      <View className="flex-1 items-center justify-center px-5 flex">
+        <FontAwesome
+          name="user-plus"
+          size={80}
+          color={
+            colorScheme === "dark" ? "white" : "black"
+          }
+        />
+        <View className="mt-4 mb-3 h-[2px] rounded-full w-[55%] bg-slate-400" />
+        <Text className="text-xl text-center w-2/3">
+          You are not signed in!
+        </Text>
+        <Text className="text-lg text-center w-3/4 mt-4">
+          Head over to <View className="text-bold">Settings</View> and create an account, or sign back in!
+        </Text>
+        <Text className="text-lg text-center w-3/4 mt-2">
+          Once signed in, you can easily sync your profile to our <View className="text-bold">Web client!</View>
+        </Text>
+      </View>
+    )
+  }
+
+  if (!auth.profile.igProfile) {
+    return (
+      <View className="h-full w-full">
         <Importer />
-		</View>
-		);
-	}
+      </View>
+    );
+  }
 
-	return (
-		<View className="flex-1 items-center pt-3 h-full w-full">
+  return (
+    <View className="flex-1 items-center pt-3 h-full w-full">
       <View>
-
-			</View>
-		</View>
-	);
+        <View>
+          <Image />
+        </View>
+      </View>
+    </View>
+  );
 }
