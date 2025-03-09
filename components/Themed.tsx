68bf0cff7d6f6477f027c0a9884dc234913ed3fe
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-03-09 21:49:23 +0000

    update dependencies, add themed pressable, adjust gitignore

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -4,82 +4,90 @@
  */
 
 import {
-	Text as DefaultText,
-	View as DefaultView,
-	SafeAreaView as DefaultSafeAreaView,
-	ScrollView as DefaultScrollView,
+  Text as DefaultText,
+  View as DefaultView,
+  SafeAreaView as DefaultSafeAreaView,
+  ScrollView as DefaultScrollView,
+  Pressable as DefaultPressable,
 } from "react-native";
 
 import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 type ThemeProps = {
-	lightColor?: string;
-	darkColor?: string;
+  lightColor?: string;
+  darkColor?: string;
 };
 
 export type TextProps = ThemeProps & DefaultText["props"];
 export type ViewProps = ThemeProps & DefaultView["props"];
 
 export function useThemeColor(
-	props: { light?: string; dark?: string },
-	colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-	const theme = useColorScheme().colorScheme ?? "light";
-	const colorFromProps = props[theme];
+  const theme = useColorScheme().colorScheme ?? "light";
+  const colorFromProps = props[theme];
 
-	if (colorFromProps) {
-		return colorFromProps;
-	} else {
-		return Colors[theme][colorName];
-	}
+  if (colorFromProps) {
+    return colorFromProps;
+  } else {
+    return Colors[theme][colorName];
+  }
 }
 
 //
 export function Text(props: TextProps) {
-	const { className, ...otherProps } = props;
-	return (
-		<DefaultText
-			className={`text-black dark:text-white ${
-				className ? className + " " : ""
-			}`}
-			{...otherProps}
-		/>
-	);
+  const { className, ...otherProps } = props;
+  return (
+    <DefaultText
+      className={`text-black dark:text-white ${className ? className + " " : ""
+        }`}
+      {...otherProps}
+    />
+  );
 }
 
 export function View(props: ViewProps) {
-	const { className, ...otherProps } = props;
-	return (
-		<DefaultView
-			className={`bg-white dark:bg-oxford-500 ${
-				className ? className + " " : ""
-			}`}
-			{...otherProps}
-		/>
-	);
+  const { className, ...otherProps } = props;
+  return (
+    <DefaultView
+      className={`bg-white dark:bg-oxford-500 ${className ? className + " " : ""
+        }`}
+      {...otherProps}
+    />
+  );
 }
 
 export function SafeAreaView(props: ViewProps) {
-	const { className, ...otherProps } = props;
-	return (
-		<DefaultSafeAreaView
-			className={`bg-white dark:bg-oxford-500 ${
-				className ? className + " " : ""
-			}`}
-			{...otherProps}
-		/>
-	);
+  const { className, ...otherProps } = props;
+  return (
+    <DefaultSafeAreaView
+      className={`bg-white dark:bg-oxford-500 ${className ? className + " " : ""
+        }`}
+      {...otherProps}
+    />
+  );
 }
 
 export function ScrollView(props: ViewProps) {
-	const { className, ...otherProps } = props;
-	return (
-		<DefaultScrollView
-			className={`bg-white dark:bg-oxford-500 ${
-				className ? className + " " : ""
-			}`}
-			{...otherProps}
-		/>
-	);
+  const { className, ...otherProps } = props;
+  return (
+    <DefaultScrollView
+      className={`bg-white dark:bg-oxford-500 ${className ? className + " " : ""
+        }`}
+      {...otherProps}
+    />
+  );
+}
+
+export function Pressable(props: ViewProps) {
+  const { className, ...otherProps } = props;
+  return (
+    <Pressable
+      className={`bg-white dark:bg-oxford-500 ${className ? className + " " : ""
+        }`}
+      {...otherProps}
+    />
+  );
 }
