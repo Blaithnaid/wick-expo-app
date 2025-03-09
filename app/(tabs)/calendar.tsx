f1cc42dd47635373e5afebd71b642804c62c4baf
Louiza <g00394926@atu.ie>
2025-03-09 17:05:07 +0000

    Calendar/agenda

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,7 +1,119 @@
-import { Text, View } from "@/components/Themed";
+import React, {Component} from 'react';
+import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
 
-export default function CalendarScreen() {
-	return (
-		<View className="flex-1 items-center justify-center h-full w-full"></View>
-	);
+
+interface State { // describes structure of the state object
+  items?: AgendaSchedule; // optional property, agendaschedule is expected type for items
 }
+
+export default class AgendaScreen extends Component<State> { 
+  state: State = { // stores components data
+    items: undefined // this holds schedule data for calendar, emtpy at start
+  };
+
+  // reservationsKeyExtractor = (item, index) => {
+  //   return `${item?.reservation?.day}${index}`;
+  // };
+
+  render() {
+    return (
+      <Agenda
+        
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems} // navigates to a new month
+        selected={'2025-09-03'} // default date when opened
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+        rowHasChanged={this.rowHasChanged}
+        showClosingKnob={true} // expand or collapse the agenda
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#43515c'},
+        //    '2017-05-09': {textColor: '#43515c'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+        // monthFormat={'yyyy'} // shows year only
+        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        // renderDay={this.renderDay}
+        // hideExtraDays={false}
+        // showOnlySelectedDayItems
+        // reservationsKeyExtractor={this.reservationsKeyExtractor}
+      />
+    );
+  }
+
+  loadItems = (day: DateData) => {
+    const items = this.state.items || {};
+
+    setTimeout(() => { // simulate loading data, adds an intentional delay
+      }, 1000);
+  };
+
+  renderDay = (day: { getDay: () => string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => {
+    if (day) {
+      return <Text style={styles.customDay}>{day.getDay()}</Text>;
+    }
+    return <View style={styles.dayItem}/>;
+  };
+
+  renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
+    const fontSize = isFirst ? 16 : 14;
+    const color = isFirst ? 'black' : '#43515c';
+
+    return (
+      <TouchableOpacity
+        
+        style={[styles.item, {height: reservation.height}]}
+        onPress={() => Alert.alert(reservation.name)}
+      >
+        <Text style={{fontSize, color}}>{reservation.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  renderEmptyDate = () => {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  };
+
+  rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
+    return r1.name !== r2.name;
+  };
+
+  timeToString(time: number) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30
+  },
+  customDay: {
+    margin: 10,
+    fontSize: 24,
+    color: 'green'
+  },
+  dayItem: {
+    marginLeft: 34
+  }
+});
