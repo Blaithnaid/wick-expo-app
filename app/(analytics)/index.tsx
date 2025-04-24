6bbda95512dc7a7684bf27664c21304a50b989d1
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-04-25 00:18:34 +0100

    continuing work on analytics

--- app/(analytics)/index.tsx
+++ app/(analytics)/index.tsx
@@ -12,10 +12,30 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 import Colors from "@/constants/Colors";
 
 export default function AnalyticsScreen() {
-	const colorScheme = useColorScheme().colorScheme;
 	const [accessToken, setAccessToken] = useState<string | null>(null);
 	const [tokenInput, setTokenInput] = useState<string>("");
+	const [overviewFigures, setOverviewFigures] = useState<
+		{ title: string; figure: number; background: string }[]
+	>([
+		{
+			title: "Total views",
+			figure: 1633,
+			background: "bg-red-400 dark:bg-red-600",
+		},
+		{
+			title: "Followers online now",
+			figure: 427,
+			background: "bg-green-400 dark:bg-green-600",
+		},
+		{ title: "Comments", figure: 14, background: "bg-sky-400 dark:bg-sky-600" },
+		{
+			title: "Followers +/-",
+			figure: 61,
+			background: "bg-orange-400 dark:bg-orange-600",
+		},
+	]);
 	const { showActionSheetWithOptions } = useActionSheet();
+	const colorScheme = useColorScheme().colorScheme;
 	const options = ["Clear token", "Cancel"];
 	const destructiveButtonIndex = 0;
 	const cancelButtonIndex = 1;
@@ -124,10 +144,10 @@ export default function AnalyticsScreen() {
 					style={{ alignItems: "center" }}
 					className="w-full px-6 flex flex-col items-center justify-center"
 				>
-					<Text className="text-xl font-bold mt-5 mb-1.5 w-full text-left">
+					<Text className="text-xl font-bold mt-5 mb-1.5 ml-2 w-full text-left">
 						Your reach this week
 					</Text>
-					<View className="w-full flex justify-center items-center pt-4 py-3 border-4 border-gray-300 dark:border-gray-500 rounded-md bg-oxford-50 dark:bg-oxford-600">
+					<View className="w-full flex justify-center items-center pt-4 py-3 border-4 border-gray-300 dark:border-gray-500 rounded-2xl bg-oxford-50 dark:bg-oxford-600">
 						<View
 							style={{ width: 320 }}
 							className="flex justify-center pt-2 items-center bg-transparent dark:bg-transparent"
@@ -164,49 +184,32 @@ export default function AnalyticsScreen() {
 										: { color: "black", textAlign: "center", fontSize: 10 }
 								}
 							/>
+							<View className="flex flex-row w-2/3 rounded-lg items-center justify-center">
+								<View className="w-1/2 p-2 rounded-l-lg">
+									<Text className="text-center">Last Week</Text>
+								</View>
+								<View className="w-1/2 p-2 rounded-r-lg">
+									<Text className="text-center">Last Week</Text>
+								</View>
+							</View>
 						</View>
 					</View>
-					<Text className="text-xl font-bold mt-4 mb-1.5 w-full text-left">
+					<Text className="text-xl font-bold mt-4 mb-1.5 ml-2 w-full text-left">
 						Overview
 					</Text>
-					<View className="h-max w-full flex flex-row flex-wrap items-center justify-center gap-2 pt-4 pb-4 px-3 border-4 border-gray-300 dark:border-gray-500 rounded-md bg-oxford-50 dark:bg-oxford-600">
-						<View className="p-6 basis-[48%] rounded-lg flex flex-col items-center justify-center bg-red-400 dark:bg-red-600">
-							<Text className="text-white text-4xl font-bold">one</Text>
-							<Text className="text-white">two</Text>
-						</View>
-						<View className="p-6 basis-[48%] rounded-lg flex items-center justify-center bg-green-400 dark:bg-green-600">
-							<Text className="text-white text-4xl font-bold">one</Text>
-							<Text className="text-white">two</Text>
-						</View>
-						<View className="p-6 basis-[48%] rounded-lg flex items-center justify-center bg-sky-400 dark:bg-sky-600">
-							<Text className="text-white text-4xl font-bold">one</Text>
-							<Text className="text-white">two</Text>
-						</View>
-						<View className="p-6 basis-[48%] rounded-lg flex items-center justify-center bg-orange-400 dark:bg-orange-600">
-							<Text className="text-white text-4xl font-bold">one</Text>
-							<Text className="text-white">two</Text>
-						</View>
-					</View>
-					<Text className="text-xl font-bold mt-4 mb-1.5 w-full text-left">
-						Overview
-					</Text>
-					<View className="h-max w-full flex flex-row flex-wrap items-center justify-center gap-2 mb-24 pt-4 pb-4 px-3 border-4 border-gray-300 dark:border-gray-500 rounded-md bg-oxford-50 dark:bg-oxford-600">
-						<View className="p-6 basis-[48%] rounded-lg flex flex-col items-center justify-center bg-red-400 dark:bg-red-600">
-							<Text className="text-white text-4xl font-bold">one</Text>
-							<Text className="text-white">two</Text>
-						</View>
-						<View className="p-6 basis-[48%] rounded-lg flex items-center justify-center bg-green-400 dark:bg-green-600">
-							<Text className="text-white text-4xl font-bold">one</Text>
-							<Text className="text-white">two</Text>
-						</View>
-						<View className="p-6 basis-[48%] rounded-lg flex items-center justify-center bg-sky-400 dark:bg-sky-600">
-							<Text className="text-white text-4xl font-bold">one</Text>
-							<Text className="text-white">two</Text>
-						</View>
-						<View className="p-6 basis-[48%] rounded-lg flex items-center justify-center bg-orange-400 dark:bg-orange-600">
-							<Text className="text-white text-4xl font-bold">one</Text>
-							<Text className="text-white">two</Text>
-						</View>
+					<View className="h-max w-full flex flex-row flex-wrap items-stretch justify-center gap-2 pt-4 pb-4 px-3 border-4 border-gray-300 dark:border-gray-500 rounded-2xl bg-oxford-50 dark:bg-oxford-600">
+						{overviewFigures.map((figure, index) => (
+							<View
+								className={`py-6 basis-[48%] rounded-lg flex flex-col items-center justify-center ${figure.background}`}
+							>
+								<Text className="text-white text-4xl font-bold">
+									{figure.figure}
+								</Text>
+								<Text className="text-white text-center text-md">
+									{figure.title}
+								</Text>
+							</View>
+						))}
 					</View>
 				</View>
 			</ScrollView>
